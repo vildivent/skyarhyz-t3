1cf9abcdc5b44addcf5f57127ac29bedb02f338d
vildivent <vildivent@mail.ru>
2023-10-09 13:34:45 +0300

    added paddings

--- src/shared/ui/PageContainer.tsx
+++ src/shared/ui/PageContainer.tsx
@@ -7,7 +7,7 @@ const PageContainer = ({
 }: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>) => {
   return (
     <main
-      className={`mx-auto flex w-full max-w-[900px] flex-col items-center lg:w-2/3 ${
+      className={`mx-auto flex w-full max-w-[900px] flex-col items-center p-2 lg:w-2/3 lg:p-5 ${
         className || ""
       }`}
       {...props}
