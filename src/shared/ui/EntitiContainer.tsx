2de0378842704cd7fccdd48731ccfd250c5cc3a1
vildivent <vildivent@mail.ru>
2023-10-09 13:47:20 +0300

    border color moved to css

--- src/shared/ui/EntitiContainer.tsx
+++ src/shared/ui/EntitiContainer.tsx
@@ -7,7 +7,7 @@ const EntitiContainer = ({
 }: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>) => {
   return (
     <div
-      className={`flex flex-col rounded-md border border-lightgray bg-darkgray/70 p-5 ${
+      className={`flex flex-col rounded-md border bg-darkgray/70 p-5 ${
         className || ""
       }`}
       {...props}
