8498a6fc816ceddea55ae6e4c7b6a6309f96c1e3
vildivent <vildivent@mail.ru>
2023-10-09 11:05:55 +0300

    added container base component

---
+++ src/shared/ui/EntitiContainer.tsx
@@ -0,0 +1,20 @@
+import type { DetailedHTMLProps, HTMLAttributes } from "react";
+
+const EntitiContainer = ({
+  children,
+  className,
+  ...props
+}: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>) => {
+  return (
+    <div
+      className={`flex flex-col rounded-md border border-lightgray bg-darkgray/70 p-5 ${
+        className || ""
+      }`}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default EntitiContainer;
