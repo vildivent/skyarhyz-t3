79505b6ac155975fa8717bc1d7dc07a3d10f11ea
vildivent <vildivent@mail.ru>
2023-10-09 13:13:44 +0300

    added layout

---
+++ src/widgets/Layout.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Navbar from "~/widgets/Navbar";
+import Footer from "~/widgets/Footer";
+
+type LayoutProps = {
+  children: JSX.Element;
+};
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <div className="fixed z-0 h-full w-full">
+        <Image
+          className="object-cover"
+          src="/bg.jpg"
+          alt="Фон"
+          fill
+          quality={100}
+        />
+      </div>
+
+      <div className="relative z-10 flex min-h-[100lvh] flex-col justify-between bg-darkgray/70">
+        <div>
+          <Navbar />
+          {children}
+        </div>
+        <Footer />
+      </div>
+
+      <div id="modal" />
+    </>
+  );
+};
+
+export default Layout;
